08241c03688078df917d2ffbd3d4e4d3356255ff
Hector Cohen <hectorcohenmaldonado@gmail.com>
2022-02-10 09:29:49 -0500

    feat:print button and table styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,63 +1,65 @@
-const monto = document.getElementById('monto');
-const tiempo = document.getElementById('tiempo');
-const interes = document.getElementById('interes');
-const btnCalcular = document.getElementById('btnCalcular');
-const alerta = document.getElementById('alert-error');
-const llenarTabla = document.querySelector('#lista-tabla tbody')
+const monto = document.getElementById("monto");
+const tiempo = document.getElementById("tiempo");
+const interes = document.getElementById("interes");
+const btnCalcular = document.getElementById("btnCalcular");
+const alerta = document.getElementById("alert-error");
+const llenarTabla = document.querySelector("#lista-tabla tbody");
 
-btnCalcular.addEventListener('click', () => {
-    if (monto.value === '' || tiempo.value === '' || interes.value === '') {
-        alerta.hidden = false;
-        setTimeout(() => {
-            alerta.hidden = true;
-        }, 2000);
-    } else {
-        calcularCronograma(monto.value, interes.value, tiempo.value);
-    }
-})
+
+btnCalcular.addEventListener("click", () => {
+  if (monto.value === "" || tiempo.value === "" || interes.value === "") {
+    alerta.hidden = false;
+    setTimeout(() => {
+      alerta.hidden = true;
+    }, 2000);
+  } else {
+    calcularCronograma(monto.value, interes.value, tiempo.value);
+  }
+});
+
+
+
 
 function calcularCronograma(monto, interes, tiempo) {
+  while (llenarTabla.firstChild) {
+    llenarTabla.removeChild(llenarTabla.firstChild);
+  }
 
-    while(llenarTabla.firstChild) {
-        llenarTabla.removeChild(llenarTabla.firstChild);
-    }
+  let mesActual = dayjs().add(1, "month");
+  let amortizacionConstante, pagoInteres, cuota;
+  amortizacionConstante = monto / tiempo;
+  for (let i = 1; i <= tiempo; i++) {
+    pagoInteres = monto * (interes / 100);
+    cuota = amortizacionConstante + pagoInteres;
+    monto = monto - amortizacionConstante;
 
-    let mesActual = dayjs().add(1, 'month');
-    let amortizacionConstante, pagoInteres, cuota;
-    amortizacionConstante = monto / tiempo;
-    for (let i = 1; i <= tiempo; i++) {
-        pagoInteres = monto * (interes / 100);
-        cuota = amortizacionConstante + pagoInteres;
-        monto = monto - amortizacionConstante;
+    let fecha = mesActual.format("DD-MM-YYYY");
+    mesActual = mesActual.add(1, "month");
 
-        let fecha = mesActual.format('DD-MM-YYYY');
-        mesActual = mesActual.add(1, 'month');
-
-        const row = document.createElement('tr');
-        row.innerHTML = `
+    const row = document.createElement("tr");
+    row.innerHTML = `
             <td>${fecha}</td>
-            <td>${Intl.NumberFormat('es-CO', {
-                style: 'currency',
-                currency: 'COP',
-                minimumFractionDigits: 0
+            <td>${Intl.NumberFormat("es-CO", {
+              style: "currency",
+              currency: "COP",
+              minimumFractionDigits: 0,
             }).format(amortizacionConstante.toFixed(2))}</td>
-            <td>${Intl.NumberFormat('es-CO', {
-                style: 'currency',
-                currency: 'COP',
-                minimumFractionDigits: 0
+            <td>${Intl.NumberFormat("es-CO", {
+              style: "currency",
+              currency: "COP",
+              minimumFractionDigits: 0,
             }).format(pagoInteres.toFixed(2))}</td>
-            <td>${Intl.NumberFormat('es-CO', {
-                style: 'currency',
-                currency: 'COP',
-                minimumFractionDigits: 0
+            <td>${Intl.NumberFormat("es-CO", {
+              style: "currency",
+              currency: "COP",
+              minimumFractionDigits: 0,
             }).format(cuota.toFixed(2))}</td>
-            <td>${Intl.NumberFormat('es-CO', {
-                style: 'currency',
-                currency: 'COP',
-                minimumFractionDigits: 0
+            <td>${Intl.NumberFormat("es-CO", {
+              style: "currency",
+              currency: "COP",
+              minimumFractionDigits: 0,
             }).format(monto.toFixed(2))}</td>
         `;
-        llenarTabla.appendChild(row);
-        
-    }
+    llenarTabla.appendChild(row);
+  }
 }
